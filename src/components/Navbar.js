2179932a0e1f151ae87d054b9165f13ea9388052
Ed Melech <mr.melech@gmail.com>
2021-10-20 15:39:28 +0100

    add button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,8 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
-  
+  const closeMobileMenu = () => setClick(false)
+
   return (
     <>
       <nav className="navbar">
@@ -16,6 +17,33 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>  
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/music' className='nav-links' onClick={closeMobileMenu}>
+                Music
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/mailing-list' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Mailing List
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
