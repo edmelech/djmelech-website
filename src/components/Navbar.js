dfc15b221b2e046edb4d08fd66ea4bf9d4ebec30
Ed Melech <mr.melech@gmail.com>
2021-10-20 12:05:17 +0100

    create handle click function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  
   return (
     <>
       <nav className="navbar">
@@ -10,7 +13,7 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             MELECH <i className="fab fa-typo3"></i> 
           </Link>  
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>  
         </div>
