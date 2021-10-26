22836e621457225ff0edadac5d37d82c24302b9f
Ed Melech <mr.melech@gmail.com>
2021-10-26 16:09:27 +0100

    import google fonts styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -20,7 +20,7 @@ export const Button = ({
    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
    return (
-     Link to='/mailing-list' className='btn-mobile'>
+     <Link to='/mailing-list' className='btn-mobile'>
       <button
       className={`btn ${checkButtonStyle} ${checkButtonSize}`}
       onClick={onClick}
@@ -28,5 +28,6 @@ export const Button = ({
       >
         {children}
       </button>
+    </Link>
    )
 };
