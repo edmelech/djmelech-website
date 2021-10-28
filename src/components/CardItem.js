50bf5819169428a475579e0fd2ce831eb876a6f6
Ed Melech <mr.melech@gmail.com>
2021-10-28 15:37:51 +0100

    fix errors

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,11 +6,11 @@ function CardItem() {
       <li className="cards__item">
         <link className="cards__item__link">
           <figure className="cards__item__pic-wrap">
-            <img src="/" alt="Travel Image"
+            <img src="/" alt="Travel"
             className="cards__item__img"/>
           </figure>
           <div className='cards__item__info'>
-            <h5 className='cards__item__text' />
+            <h5 className='cards__item__text'>PLACE HOLDER</h5>
           </div>
         </link>
       </li>
