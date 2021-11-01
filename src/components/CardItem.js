515273cadd5cea81922d32ec9482690f3fced050
Ed Melech <mr.melech@gmail.com>
2021-11-01 15:50:37 +0000

    corrct Link To links

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CardItem() {
   return (
     <>
       <li className="cards__item">
-        <link className="cards__item__link">
+        <Link className="cards__item__link">
           <figure className="cards__item__pic-wrap">
             <img src="/" alt="Travel"
             className="cards__item__img"/>
           </figure>
           <div className='cards__item__info'>
-            <h5 className='cards__item__text'>PLACE HOLDER</h5>
+            <h5 className='cards__item__text' />
           </div>
-        </link>
+        </Link>
       </li>
     </>
   )
