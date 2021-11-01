9dfda4466e66f17065c105eedec81acc8a2fec8b
Ed Melech <mr.melech@gmail.com>
2021-11-01 17:57:29 +0000

    make new pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Music from './components/pages/Music';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+import MailingList from './components/pages/MailingList';
 
 function App() {
   return (
@@ -10,8 +14,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component=
-          {Home} />
+          <Route path='/' exact component={Home} />
+          <Route path='/music' exact component={Music} />
+          <Route path='/contact' exact component={Contact} />
+          <Route path='/about' exact component={About} />
+          <Route path='/mailing-list' exact component={MailingList} />
         </Switch>  
       </Router>
     </>
---
+++ src/components/pages/MailingList.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../../App.css'
+
+export default function MailingList() {
+  return <h1 className="mailing-list">MAILING LIST</h1>
+}
