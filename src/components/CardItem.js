3bd62784b813a1f5eebfea3841a9f5619cf6d488
Ed Melech <mr.melech@gmail.com>
2021-11-02 19:51:53 +0000

    gst

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function CardItem(props) {
-  console.log(props.src)
   return (
     <>
       <li className="cards__item">
         <Link className="cards__item__link" to={props.path}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="DJ image"
+            <img src={props.src} alt="DJ"
             className="cards__item__img"/>
           </figure>
           <div className='cards__item__info'>
