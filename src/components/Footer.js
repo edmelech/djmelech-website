e3d1c12989db8ba74ab7d60ec3865aa1726bcd7e
Ed Melech <mr.melech@gmail.com>
2021-11-15 20:36:13 +0000

    add CSS stylinh to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Button } from './Button'
+import React from 'react';
 import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
 
 function Footer() {
   return (
