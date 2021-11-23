10d1f521b49ed2841fb8ffaf45d4900eede9f9fe
Ed Melech <mr.melech@gmail.com>
2021-11-23 19:51:48 +0000

    added headphone icon to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick=
           {closeMobileMenu}>
-            MELECH <i className="fab fa-typo3"></i> 
+            MELECH <i class="fas fa-headphones-alt"></i>
           </Link>  
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
