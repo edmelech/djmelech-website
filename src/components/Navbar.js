76279d99fbb1f2ad8e100f91d893be3e09d9e7e2
Ed Melech <mr.melech@gmail.com>
2021-11-25 12:15:20 +0000

    change font and make logo bigger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick=
           {closeMobileMenu}>
-            MELECH <i class="fas fa-headphones-alt"></i>
+            MELECH 
           </Link>  
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
