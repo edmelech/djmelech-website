b2113ec2d08a071f6857072918106f7981e6e8c6
Ed Melech <mr.melech@gmail.com>
2021-12-14 15:17:00 +0000

    add about card

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,5 +2,18 @@ import React from 'react'
 import '../../App.css'
 
 export default function About() {
-  return <h1 className="about">ABOUT</h1>
+  return (
+    <div className="about">
+      <h1 className="about-card">
+        Ed Melech (also known as Cyrus to some) is a resident DJ for some of London's top venues, including Loulou's, Sketch, The Ned and Folie. 
+        
+        For over 10 years he has been playing regularly across the London circuit as well private parties throughout the UK and Europe. 
+
+        <br/><br/>
+
+        Constantly searching for new music, Melech has a love for all genres with his DJ sets focusing on underground house, disco and beyond.
+
+      </h1>
+    </div>
+  ) 
 }
