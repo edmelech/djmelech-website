0e80500535e04d4966e007312fa4eaec3e76d971
Ed Melech <mr.melech@gmail.com>
2021-12-21 16:02:52 +0000

    update hyperlinks

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,9 +7,9 @@ export default function Contact() {
       <div className="contact_card">
           ED@DJMELECH.COM<br/><br/>
           <a href="https://www.instagram.com/dj_melech/" class="custom-link">INSTAGRAM</a><br/>
-          <a href="https://www.instagram.com/dj_melech/" class="custom-link">FACEBOOK</a><br/>
-          <a href="https://www.instagram.com/dj_melech/" class="custom-link">TWITTER</a><br/>
-          <a href="https://www.instagram.com/dj_melech/" class="custom-link">RESIDENT ADVISOR</a><br/>
+          <a href="https://www.facebook.com/DJMelech" class="custom-link">FACEBOOK</a><br/>
+          <a href="https://twitter.com/EdMelech" class="custom-link">TWITTER</a><br/>
+          <a href="https://ra.co/dj/melech" class="custom-link">RESIDENT ADVISOR</a><br/>
       </div>
     </div>
   );
