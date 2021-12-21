123f17fa9a89b1d6555d280b9ce0f9f4abc0f699
Ed Melech <mr.melech@gmail.com>
2021-12-21 15:46:07 +0000

    change background image for contact page. Add more links

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,8 +6,10 @@ export default function Contact() {
     <div className="contact">
       <div className="contact_card">
           ED@DJMELECH.COM<br/><br/>
-          <a href="https://www.instagram.com/dj_melech/">INSTAGRAM</a><br/>
-          
+          <a href="https://www.instagram.com/dj_melech/" class="custom-link">INSTAGRAM</a><br/>
+          <a href="https://www.instagram.com/dj_melech/" class="custom-link">FACEBOOK</a><br/>
+          <a href="https://www.instagram.com/dj_melech/" class="custom-link">TWITTER</a><br/>
+          <a href="https://www.instagram.com/dj_melech/" class="custom-link">RESIDENT ADVISOR</a><br/>
       </div>
     </div>
   );
