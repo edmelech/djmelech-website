a260c841f516864ae8b5105125810d62c1147288
Ed Melech <mr.melech@gmail.com>
2021-12-21 16:54:44 +0000

    open links in new tab

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-card">
-        Ed Melech (also known as Cyrus to some) is a resident DJ for some of London's top venues, including Loulou's, Sketch, The Ned and Folie. 
+        Ed Melech (also known as Cyrus to some) is a resident DJ for London venues Loulou's, Sketch, The Ned, Folie and El Camion. 
         <br/><br/>
-        For over 10 years he has been playing regularly across the London circuit as well private parties throughout the UK and Europe. 
+        For over 10 years he has been playing regularly across the London circuit as well as private parties throughout the UK and Europe. In 2021 these included parties in a medieval castle and a Scillian grotto. 
         <br/><br/>
-        Constantly searching for new music, Melech has a love for all genres, with his DJ sets focusing on underground house, disco and beyond. 
+        Constantly searching for new tunes and forgotten classics, Melech focuses his DJ sets on underground house, disco and beyond. 
 
       </div>
     </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,11 +5,11 @@ export default function Contact() {
   return (
     <div className="contact">
       <div className="contact_card">
-          ED@DJMELECH.COM<br/><br/>
-          <a href="https://www.instagram.com/dj_melech/" class="custom-link">INSTAGRAM</a><br/>
-          <a href="https://www.facebook.com/DJMelech" class="custom-link">FACEBOOK</a><br/>
-          <a href="https://twitter.com/EdMelech" class="custom-link">TWITTER</a><br/>
-          <a href="https://ra.co/dj/melech" class="custom-link">RESIDENT ADVISOR</a><br/>
+          ed@djmelech.com<br/><br/>
+          <a href="https://www.instagram.com/dj_melech/" target="_blank" class="custom-link">INSTAGRAM</a><br/>
+          <a href="https://www.facebook.com/DJMelech" target="_blank" class="custom-link">FACEBOOK</a><br/>
+          <a href="https://twitter.com/EdMelech" target="_blank" class="custom-link">TWITTER</a><br/>
+          <a href="https://ra.co/dj/melech" target="_blank" class="custom-link">RESIDENT ADVISOR</a><br/>
       </div>
     </div>
   );
