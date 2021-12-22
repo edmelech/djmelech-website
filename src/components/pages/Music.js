3d9eb4414db8cad687f6c51c10938d6cbd990a1c
Ed Melech <mr.melech@gmail.com>
2021-12-22 17:01:00 +0000

    add soundcloud player to music page

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import '../../App.css'
 import Cards from '../Cards';
+import ReactPlayer from 'react-player';
 
-export default function Music() {
-  return <h1 className="music">Music</h1>  
+function Music() {
+  return (
+    <h1 className="music">
+      <div>
+      <ReactPlayer
+        url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"/>
+      </div>
+    </h1>  
+    
+    
+  ) 
 }
+
+export default Music
