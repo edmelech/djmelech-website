c7e7284dcc6f42f64ee6b074de7def91c334a73a
Ed Melech <mr.melech@gmail.com>
2021-12-29 16:24:44 +0000

    add 2nd react player

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -7,11 +7,18 @@ function Music() {
   return (
     <h1 className="music">
       <div className='player-container'>
-      <ReactPlayer
-        className='react-player'
-        url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-        width='600px'
-        height='200px'
+        <ReactPlayer
+          className='react-player'
+          url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+          width='600px'
+          height='200px'
+        />
+      
+        <ReactPlayer
+          className='react-player'
+          url="https://soundcloud.com/drmrllpls/melechtric-mix-feb-2018?si=a758dabc2b0f45bf87a99a492f05284b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+          width='600px'
+          height='200px'
         />
       </div>
     </h1>  
