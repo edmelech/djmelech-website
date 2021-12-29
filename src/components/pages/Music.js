9563335be5012236c6e934474d0f99e1ae355918
Ed Melech <mr.melech@gmail.com>
2021-12-29 16:19:19 +0000

    style react player

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -6,9 +6,13 @@ import ReactPlayer from 'react-player';
 function Music() {
   return (
     <h1 className="music">
-      <div>
+      <div className='player-container'>
       <ReactPlayer
-        url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"/>
+        className='react-player'
+        url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+        width='600px'
+        height='200px'
+        />
       </div>
     </h1>  
     
