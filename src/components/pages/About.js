7c848c6f15ac1406313f784b31a377a2a6abb359
Ed Melech <mr.melech@gmail.com>
2021-12-29 17:08:20 +0000

    add space between soundcloud players.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-card">
-        Ed Melech (also known as Cyrus to some) is a resident DJ for London venues Loulou's, Sketch, The Ned, Folie and El Camion. 
+        Ed Melech (also known by his Persian name Cyrus) is a resident DJ for London venues Loulou's, Sketch, The Ned, Folie and El Camion. 
         <br/><br/>
         For over 10 years he has been playing regularly across the London circuit as well as private parties throughout the UK and Europe. 
         <br/><br/>
--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -11,14 +11,14 @@ function Music() {
           className='react-player'
           url="https://soundcloud.com/djmelech/april-2021-dj-mix?si=3a579859e22b4815a269a0d782a36fb9&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
           width='600px'
-          height='200px'
+          height='190px'
         />
-      
+        <br/>
         <ReactPlayer
           className='react-player'
           url="https://soundcloud.com/drmrllpls/melechtric-mix-feb-2018?si=a758dabc2b0f45bf87a99a492f05284b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
           width='600px'
-          height='200px'
+          height='190px'
         />
       </div>
     </h1>  
