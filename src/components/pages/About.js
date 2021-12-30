346c37bc228a0074875643c2108ef2da4d27ced8
Ed Melech <mr.melech@gmail.com>
2021-12-30 14:56:22 +0000

    update bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,11 +7,11 @@ export default function About() {
       <div className="about-card">
         Ed Melech (also known by his Persian name Cyrus) is a resident DJ for London venues Loulou's, Sketch, The Ned, Folie and El Camion. 
         <br/><br/>
-        For over 10 years he has been playing regularly across the London circuit as well as private parties throughout the UK and Europe. 
+        For over 10 years he has played regularly across the London circuit as well as private parties throughout the UK and Europe. 
         <br/><br/>
-        Melech previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out and also take the brand to Bestival. Between 2018-2020 Melech hosted the DRM RLL PLS show on Soho Radio. 
+        Melech previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, play Bestival and host a show on Soho Radio between 2018-2020. 
         <br/><br/>
-        Constantly searching for new tunes and forgotten classics, Melech focuses his DJ sets on underground house, disco and beyond. 
+        Constantly searching for new tunes and forgotten classics, Melech focuses his DJ sets on underground house, techno, disco and beyond. 
 
       </div>
     </div>
