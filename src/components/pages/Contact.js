70ce844e43f90eb436f8ae9135cbf6094c2ddedc
Ruben Swieringa <ruben.swieringa@gmail.com>
2022-01-16 13:49:51 +0000

    Turn email address on contact-page into mailto-link

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div className="contact">
       <div className="contact_card">
-          ed@djmelech.com<br/><br/>
+          <a href="mailto:ed@djmelech.com" class="custom-link">ed@djmelech.com</a><br/><br/>
           <a href="https://www.instagram.com/dj_melech/" target="_blank" class="custom-link">INSTAGRAM</a><br/>
           <a href="https://www.facebook.com/DJMelech" target="_blank" class="custom-link">FACEBOOK</a><br/>
           <a href="https://twitter.com/EdMelech" target="_blank" class="custom-link">TWITTER</a><br/>
