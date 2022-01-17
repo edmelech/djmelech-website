b4d5e6c8f5becaa3d4ca564cd46f51f01895631c
Ed Melech <mr.melech@gmail.com>
2022-01-17 18:08:29 +0000

    remove React Player component from music page

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -1,24 +1,11 @@
 import React from 'react'
 import '../../App.css'
 import Cards from '../Cards';
-import ReactPlayer from 'react-player';
 
 function Music() {
   return (
     <h1 className="music">
       <div className='player-container'>
-        {/* <ReactPlayer
-          className='react-player'
-          url="https://soundcloud.com/djmelech/april-2021-dj-mix"
-          width='300px'
-          height='190px'
-        /> */}
-
-        {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1025532784&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-          <div style={{fontSize: '10px', color: '#cccccc',lineBreak: 'anywhere',wordBreak: 'normal',overflow: 'hidden',whiteSpace: 'nowrap',textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',fontWeight: 100}}>
-          <a href="https://soundcloud.com/djmelech" title="Melech" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Melech</a> · <a href="https://soundcloud.com/djmelech/april-2021-dj-mix" title="April 2021 DJ Mix" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>April 2021 DJ Mix</a></div>
-         */}
-
         <iframe 
           width="90%" 
           height="120" 
