7b5e81621caa969bfa0649785522a54eab3d159d
Ed Melech <mr.melech@gmail.com>
2022-01-19 16:19:38 +0000

    flexbox items on music page need work

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-card">
-        Ed Melech (also known by his Persian name Cyrus) is a resident DJ for London venues Loulou's, Sketch, The Ned, Folie and El Camion. 
+        Ed Melech (also known by his Persian name Cyrus) is a resident DJ for London venues Loulou's, Sketch, The Ned and Folie. 
         <br/><br/>
         For over 10 years he has played regularly across the London circuit as well as private parties throughout the UK and Europe. 
         <br/><br/>
-        Melech previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, play Bestival and host a show on Soho Radio between 2018-2020. 
+        Melech previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, playing alongside DJs such as Tiga, Basement Jaxx and Flying Lotus. He also took DRM RLL PLS to Bestival and hosted a show on Soho Radio between 2018-2020. 
         <br/><br/>
         Constantly searching for new tunes and forgotten classics, Melech focuses his DJ sets on underground house, techno, disco and beyond. 
 
--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -4,31 +4,56 @@ import '../../App.css'
 function Music() {
   return (
     <h1 className="music">
-      <div className='player-container'>
-        <div className="mix-container">
-        <iframe 
-          width="90%" 
-          height="120" 
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2FDJMelech%2Fmelech-disco-mix-jan-2022%2F" 
-          frameborder="0"
-          title="DJ Melech Disco Mix January 2022">
-        </iframe>  
+      <div className='Player-tracklist-container'>
+        <div className='player-container'>
+          <div className="mix-container">
+          <iframe 
+            width="90%" 
+            height="120" 
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2FDJMelech%2Fmelech-disco-mix-jan-2022%2F" 
+            frameborder="0"
+            title="DJ Melech Disco Mix January 2022">
+          </iframe>  
+          </div>
+          
+          <br/>
+          <div className="mix-info">
+            <p>Welcome to my music page. Above is my latest mix which I made to go with the launch of this site. It features a load of music from my DJ sets over the past few weeks.</p>
+            <br></br> 
+            <p>I'm working on a house mix and will get that online soon. I'm also working on my own original music so watch this space.</p>
+            <br></br>
+            <p>
+            To listen to my archive of mixes and radio shows, head over to the <a href="https://www.mixcloud.com/drmrllpls" target="_blank" rel="noreferrer" class="bio-link">DRM RLL PLS Mixcloud</a>  page. All future mixes will be available on this page.
+            </p>
+          </div>      
         </div>
-
-        
-
-        <br/>
-        <div className="mix-info">
-          <p>Welcome to my music page. Above is my latest mix which I made to go with the launch of this site. It features a load of music from my DJ sets over the past few weeks.</p>
-          <br></br> 
-          <p>I'm working on a house mix and will get that online soon. I'm also working on my own original music so watch this space.</p>
+        <div className='tracklist'>
+        <p>
+          <strong>Melech Disco Mix - Jan 2022</strong>
           <br></br>
-          <p>
-          To listen to my archive of mixes and radio shows, head over to the <a href="https://www.mixcloud.com/drmrllpls" target="_blank" rel="noreferrer" class="bio-link">DRM RLL PLS Mixcloud</a>  page. All future mixes will be available on this page.
-          </p>
+          <br></br>Baris K - Aheste
+          <br></br>MMT* - Sevmek Istiyorum
+          <br></br>Chuck Brown and The Soul Searchers - Bustin' Loose
+          <br></br>Pamela Nivens - It's You I Love (Instrumental Mix)
+          <br></br>Candi Staton - When You Wake Up Tomorrow
+          <br></br>Sylvester - Tell Me (Remix)
+          <br></br>Evo - Din Don (Instrumental)
+          <br></br>T Connection - At Midnight
+          <br></br>Nu Genea - Je Vulesse
+          <br></br>Pellegrino & Zodyaco - Amaremai
+          <br></br>Claudja Barry - Sweet Dynamite (Todd Terje edit)
+          <br></br>Japan - Life In Tokyo
+          <br></br>Schatzi Edits - A2 Schatzi 007
+          <br></br>Pigeon - Yagana
+          <br></br>Schatzi Edits - A1 Schatzi 009
+          <br></br>The Vision - Heaven
+          <br></br>D.C La Rue - Cathedrals (Jamie 3:26 Extended Disco Dub)
+          <br></br>Tom Moulton - Small Circle of Friends
+          <br></br>Christopher Cross - Ride Like The Wind (Joey Negro Extended Disco Mix)
+          <br></br>Asso - Do It Again</p>
         </div>
-               
       </div>
+
     </h1>  
     
     
