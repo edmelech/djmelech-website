515bcb7ed0711eed6c27db4064d2cd9d90a0144f
Ed Melech <mr.melech@gmail.com>
2022-01-20 15:50:02 +0000

    change font on contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,11 +5,12 @@ export default function Contact() {
   return (
     <div className="contact">
       <div className="contact_card">
-          <a href="mailto:ed@djmelech.com" class="custom-link">ed@djmelech.com</a><br/><br/>
+          <a href="mailto:ed@djmelech.com" class="custom-link">ED@DJMELECH.COM</a><br/><br/>
           <a href="https://www.instagram.com/dj_melech/" target="_blank" rel="noreferrer" class="custom-link">INSTAGRAM</a><br/>
           <a href="https://www.facebook.com/DJMelech" target="_blank" rel="noreferrer" class="custom-link">FACEBOOK</a><br/>
           <a href="https://twitter.com/EdMelech" target="_blank" rel="noreferrer" class="custom-link">TWITTER</a><br/>
           <a href="https://ra.co/dj/melech" target="_blank" rel="noreferrer" class="custom-link">RESIDENT ADVISOR</a><br/>
+          <a href="https://soundcloud.com/djmelech" target="_blank" rel="noreferrer" class="custom-link">SOUNDCLOUD</a><br/>
       </div>
     </div>
   );
