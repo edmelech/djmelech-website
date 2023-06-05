e6766a02bcbbecd2aa6a4a9a1ee8ccb297cb353c
Ed Melech <mr.melech@gmail.com>
2023-06-05 13:28:45 +0100

    updated music section

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -6,6 +6,17 @@ function Music() {
     <h1 className="music">
       <div className='Player-tracklist-container'>
         <div className='player-container'>
+
+          <div className="mix-container">
+            <iframe 
+              width="100%" 
+              height="120" 
+              src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2FDJMelech%2Fdj-melech-deep-melodic-house-mix-june-2023%2F" 
+              frameborder="0"
+              title="DJ Melech Deep / Melodic House Mix June 2023">
+            </iframe>  
+          </div>
+
           <div className="mix-container">
           <iframe 
             width="100%" 
@@ -29,35 +40,12 @@ function Music() {
           <br/>
           <div className="mix-info">
             <p>
-              To listen to my archive of mixes and radio shows, head over to the <a href="https://www.mixcloud.com/drmrllpls" target="_blank" rel="noreferrer" class="bio-link">DRM RLL PLS Mixcloud</a>  page. All future mixes will be available on this page.
+              To listen to my archive of mixes and radio shows, head over to the <a href="https://www.mixcloud.com/drmrllpls" target="_blank" rel="noreferrer" class="bio-link">DRM RLL PLS Mixcloud</a>  page. 
+              <br/><br/>
+              Follow my <a href="https://open.spotify.com/playlist/3VKFQPBeqxfEjaMrjwHhgw?si=fee7ec6a942a4941" target="_blank" rel="noreferrer" class="bio-link">Spotify Playlist</a> to keep up to date with tunes I am playing in my sets each week.
             </p>
           </div>      
         </div>
-        {/* <div className='tracklist'>
-        <p>
-          <strong>Melech Disco Mix - Jan 2022</strong>
-          <br></br>
-          <br></br>Baris K - Aheste
-          <br></br>MMT* - Sevmek Istiyorum
-          <br></br>Chuck Brown and The Soul Searchers - Bustin' Loose
-          <br></br>Pamela Nivens - It's You I Love (Instrumental Mix)
-          <br></br>Candi Staton - When You Wake Up Tomorrow
-          <br></br>Sylvester - Tell Me (Remix)
-          <br></br>Evo - Din Don (Instrumental)
-          <br></br>T Connection - At Midnight
-          <br></br>Nu Genea - Je Vulesse
-          <br></br>Pellegrino & Zodyaco - Amaremai
-          <br></br>Claudja Barry - Sweet Dynamite (Todd Terje edit)
-          <br></br>Japan - Life In Tokyo
-          <br></br>Schatzi Edits - A2 Schatzi 007
-          <br></br>Pigeon - Yagana
-          <br></br>Schatzi Edits - A1 Schatzi 009
-          <br></br>The Vision - Heaven
-          <br></br>D.C La Rue - Cathedrals (Jamie 3:26 Extended Disco Dub)
-          <br></br>Tom Moulton - Small Circle of Friends
-          <br></br>Christopher Cross - Ride Like The Wind (Joey Negro Extended Disco Mix)
-          <br></br>Asso - Do It Again</p>
-        </div> */}
       </div>
 
     </h1>  
