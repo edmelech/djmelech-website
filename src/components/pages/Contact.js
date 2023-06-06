4c968ad73e6473511877c66876423588bf04aad8
Ed Melech <mr.melech@gmail.com>
2023-06-06 12:53:58 +0100

    update email and footer

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div className="contact">
       <div className="contact_card">
-          <a href="mailto:ed@djmelech.com" class="custom-link">ED@DJMELECH.COM</a><br/><br/>
+          <a href="mailto:ed@djmelech.com" class="custom-link">INFO@DJMELECH.COM</a><br/><br/>
           <a href="https://www.instagram.com/dj_melech/" target="_blank" rel="noreferrer" class="custom-link">INSTAGRAM</a><br/>
           <a href="https://www.facebook.com/DJMelech" target="_blank" rel="noreferrer" class="custom-link">FACEBOOK</a><br/>
           <a href="https://twitter.com/EdMelech" target="_blank" rel="noreferrer" class="custom-link">TWITTER</a><br/>
