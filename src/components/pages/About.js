80803f7d5288e4244bbf5cd1c5861a8ecb843009
Ed Melech <mr.melech@gmail.com>
2023-08-29 13:29:09 +0100

    updated bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-card">
-        <p>DJ Melech (AKA Cyrus) is a resident DJ for London venues Loulou's, Maison Estelle, Sketch, The Ned and The Treehouse Hotel.</p> 
+        <p>DJ Melech (AKA Cyrus) is a resident DJ for London venues House of Koko, Maison Estelle, Sketch, The Ned and The Nest.</p> 
         <p>For over 15 years he has played regularly across the London circuit as well as private parties throughout the UK and Europe.</p> 
         <p>Cyrus previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, playing alongside DJs such as Tiga, Basement Jaxx and Flying Lotus. He also took DRM RLL PLS to Bestival and hosted a show on Soho Radio between 2018-2020.</p> 
-        <p>Constantly searching for new tunes and forgotten classics, Cyrus focuses his DJ sets on underground house, techno, disco and beyond. His sets also feature his own own productions and edits, making them truly unique from anyone else. </p> 
+        <p>Constantly searching for new tunes and forgotten classics, Cyrus focuses his DJ sets on underground house, techno, disco and beyond. His sets also feature his own productions and edits, making them truly unique from anyone else. </p> 
       </div>
     </div>
   ) 
