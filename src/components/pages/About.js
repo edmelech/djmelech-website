57900b2de54e9ccc1ccad2460aa26c3c3c885360
Ed Melech <mr.melech@gmail.com>
2025-07-28 16:49:30 +0100

    updated bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,9 +5,9 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-card">
-        <p>DJ Melech (Cyrus to his friends) is a resident DJ for London Chiltern Firehouse, Loulou's, Nikita and Sketch.</p> 
+        <p>DJ Melech (Cyrus to his friends) is a resident DJ for London Chiltern Firehouse, CoOc, Nikita and Gallery.</p> 
         <p>For over 15 years he has played regularly across the London circuit as well as private parties throughout the UK and Europe.</p> 
-        <p>Cyrus previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, playing alongside DJs such as Tiga, Basement Jaxx and Flying Lotus. He also took DRM RLL PLS to Bestival and hosted a show on Soho Radio between 2018-2020.</p> 
+        <p>Cyrus previously ran the DRM RLL PLS club night which saw him host room 2 at Bugged Out, playing alongside DJs such as Tiga, Basement Jaxx and Flying Lotus. He also took DRM RLL PLS to Bestival and hosted a show on Soho Radio between 2018-2020. 2025 saw Cyrus return to radio with some guest shows on London's Voice Radio.</p> 
         <p>Constantly searching for new tunes and forgotten classics, Cyrus focuses his DJ sets on underground house, techno, disco and beyond. His sets also feature his own productions and edits, making them truly unique from anyone else. </p> 
       </div>
     </div>
