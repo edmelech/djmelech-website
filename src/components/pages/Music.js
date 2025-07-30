403b8f94a71d8afdb52983700026866110e637db
Ed Melech <mr.melech@gmail.com>
2025-07-30 17:39:23 +0100

    added border to top mix

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -13,6 +13,7 @@ function Music() {
                 title = "Mixcloud player" 
                 width="100%" 
                 height="120" 
+                frameborder="0"
                 src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2FDJMelech%2Fdj-melech-mix-march-2024%2F">
               </iframe>
             </div>
